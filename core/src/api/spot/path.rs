54d31d2709e49eb4fe1add4327817f8eaef6bcce
Denis Gruia <denis.gruiax@icloud.com>
2025-04-19 00:11:46 +0300

    Added Spot Market endpoints

--- core/src/api/spot/path.rs
+++ core/src/api/spot/path.rs
@@ -1,8 +1,7 @@
-pub enum General{
+pub enum General {
     TestConnectivity,
     CheckServerTime,
     ExchangeInformation,
-    
 }
 
 impl From<General> for &'static str {
@@ -11,6 +10,40 @@ impl From<General> for &'static str {
             General::TestConnectivity => "/api/v3/ping",
             General::CheckServerTime => "/api/v3/time",
             General::ExchangeInformation => "/api/v3/exchangeInfo",
+        }
+    }
+}
+
+pub enum Market {
+    OrderBook,
+    RecentTradeList,
+    OldTradeLookup,
+    CompressedAggregateTradesList,
+    KlineCandlestickData,
+    UIKlines,
+    CurrentAveragePrice,
+    TickerPriceChangeStatistics24Hr,
+    TradingDayTicker,
+    SymbolPriceTicker,
+    SymbolOrderBookTicker,
+    RollingWindowPriceChangeStatistics,
+}
+
+impl From<Market> for &'static str {
+    fn from(value: Market) -> Self {
+        match value {
+            Market::OrderBook => "/api/v3/depth",
+            Market::RecentTradeList => "/api/v3/trades",
+            Market::OldTradeLookup => "/api/v3/historicalTrades",
+            Market::CompressedAggregateTradesList => "/api/v3/aggTrades",
+            Market::KlineCandlestickData => "/api/v3/klines",
+            Market::UIKlines => "/api/v3/uiKlines",
+            Market::CurrentAveragePrice => "/api/v3/avgPrice",
+            Market::TickerPriceChangeStatistics24Hr => "/api/v3/ticker/24hr",
+            Market::TradingDayTicker => "/api/v3/ticker/tradingDay",
+            Market::SymbolPriceTicker => "/api/v3/ticker/price",
+            Market::SymbolOrderBookTicker => "/api/v3/ticker/bookTicker",
+            Market::RollingWindowPriceChangeStatistics => "/api/v3/ticker",
         }
     }
 }

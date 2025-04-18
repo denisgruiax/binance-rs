8d9f6d673f5d229ddba8dfd51f043f886b96e3d1
Denis Gruia <denis.gruiax@icloud.com>
2025-04-18 23:59:00 +0300

    Added rest of the Spot General endpoints

--- core/src/api/spot/path.rs
+++ core/src/api/spot/path.rs
@@ -1,11 +1,16 @@
 pub enum General{
-    TestConnectivity
+    TestConnectivity,
+    CheckServerTime,
+    ExchangeInformation,
+    
 }
 
 impl From<General> for &'static str {
     fn from(value: General) -> Self {
         match value {
-            General::TestConnectivity => "/api/v3/ping"
+            General::TestConnectivity => "/api/v3/ping",
+            General::CheckServerTime => "/api/v3/time",
+            General::ExchangeInformation => "/api/v3/exchangeInfo",
         }
     }
 }

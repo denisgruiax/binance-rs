d0cec68d9d78fe4fe6526dc266bad931b8ae7411
Denis Gruia <denis.gruiax@icloud.com>
2025-04-19 01:49:53 +0300

    Added Serialize to Spot General models

--- core/src/model/spot/general.rs
+++ core/src/model/spot/general.rs
@@ -1,4 +1,43 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize)]
 pub struct Empty {}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ServerTime {
+    pub server_time: u64,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExchangeInformation {
+    pub timezone: String,
+    pub server_time: u64,
+    pub rate_limits: Vec<RateLimit>,
+    pub symbols: Vec<Symbol>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct RateLimit {
+    pub rate_limit_type: String,
+    pub interval: String,
+    pub interval_num: u16,
+    pub limit: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Symbol {
+    pub symbol: String,
+    pub status: String,
+    pub base_asset: String,
+    pub base_asset_precision: u64,
+    pub quote_asset: String,
+    pub order_types: Vec<String>,
+    pub iceberg_allowed: bool,
+    pub is_spot_trading_allowed: bool,
+    pub is_margin_trading_allowed: bool,
+    //pub filters: Vec<Filters>,
+}

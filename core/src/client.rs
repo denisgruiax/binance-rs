5c01aaef547ffe20e74faefc2aeb59b78a31d803
Denis Gruia <denis.gruiax@icloud.com>
2025-04-21 18:55:23 +0300

    Core module for clients

--- core/src/client.rs
+++ core/src/client.rs
@@ -1,26 +1,3 @@
-use reqwest::Response;
-pub struct Client<'a> {
-    host: &'a str,
-    api_key: &'a str,
-    secret_key: &'a str,
-    inner_client: reqwest::Client,
-}
-
-impl<'a> Client<'a> {
-    pub fn new(host: &'a str, api_key: &'a str, secret_key: &'a str) -> Client<'a> {
-        Client {
-            host,
-            api_key,
-            secret_key,
-            inner_client: reqwest::Client::new(),
-        }
-    }
-
-    pub async fn get<T>(&self, path: T) -> impl Future<Output = Result<Response, reqwest::Error>>
-    where
-        T: Into<&'static str>,
-    {
-        let endpoint = format!("{}{}", self.host, path.into());
-        self.inner_client.get(endpoint).send()
-    }
-}
+pub mod asynchronous;
+pub mod signer;
+pub mod synchronous;

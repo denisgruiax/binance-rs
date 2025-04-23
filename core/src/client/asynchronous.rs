21fac71dc68b5f64d0988db8a3cba09f93e0c443
Denis Gruia <denis.gruiax@icloud.com>
2025-04-23 18:24:02 +0300

    Updated the asynchronous client

--- core/src/client/asynchronous.rs
+++ core/src/client/asynchronous.rs
@@ -3,29 +3,23 @@ use reqwest::Response;
 
 pub struct Client<'a, S>
 where
-    S: Signature,
+    S: Signature<'a>,
 {
     host: &'a str,
-    api_key: &'a str,
-    secret_key: &'a str,
-    signature: &'a S,
+    signature: S,
     inner_client: reqwest::Client,
 }
 
 impl<'a, S> Client<'a, S>
 where
-    S: Signature,
+    S: Signature<'a>,
 {
     pub fn new(
         host: &'a str,
-        api_key: &'a str,
-        secret_key: &'a str,
-        signature: &'a S,
+        signature: S,
     ) -> Client<'a, S> {
         Client {
             host,
-            api_key,
-            secret_key,
             signature,
             inner_client: reqwest::Client::new(),
         }

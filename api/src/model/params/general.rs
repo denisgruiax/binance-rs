c6fb092af8e36f94299f745a9d3691dda71d2bdc
Denis Gruia <denis.gruiax@icloud.com>
2025-04-25 15:46:46 +0300

    Implemented ExchangeInformationParams for building the parameters into an url encoded String

--- api/src/model/params/general.rs
+++ api/src/model/params/general.rs
@@ -1,5 +1,28 @@
-pub struct ExchangeInformationParams{
-    pub symbol: Option<String>,
-    pub symbols: Option<String>,
-    pub permissions: Option<Vec<String>>,
+use std::fmt::Display;
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ExchangeInformationParams<'a> {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub symbol: Option<&'a str>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub symbols: Option<&'a str>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub permissions: Option<&'a str>,
+
+    #[serde(rename = "recvWindow")]
+    pub recv_window: u32,
+}
+
+impl<'a> Display for ExchangeInformationParams<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            serde_urlencoded::to_string(self).unwrap_or_else(|_| String::new())
+        )
+    }
 }

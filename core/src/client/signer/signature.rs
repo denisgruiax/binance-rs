a347ff64ca67372f276ff45eb175a8d435ebf472
Denis Gruia <denis.gruiax@icloud.com>
2025-04-26 11:24:19 +0300

    Signature trait now contains add timestamp, build request and sign functions

--- core/src/client/signer/signature.rs
+++ core/src/client/signer/signature.rs
@@ -1,3 +1,10 @@
+use chrono::Utc;
+
 pub trait Signature<'a> {
-    fn sign(&self, params: &str) -> String;
+    fn add_timestamp(&self, params: &str) -> String {
+        format!("{}&timestamp={}", params, Utc::now().timestamp_millis())
+    }
+
+    fn build_request(&self, host: &str, path: &str, params: &str) -> reqwest::RequestBuilder;
+    fn sign(&self, host: &str, path: &str, params: &str) -> String;
 }

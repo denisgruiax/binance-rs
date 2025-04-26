164721636c5449e6982a8b70e79f418f197a5316
Denis Gruia <denis.gruiax@icloud.com>
2025-04-26 10:20:46 +0300

    Converted secret_key to private field

--- core/src/client/signer/hmacsha256.rs
+++ core/src/client/signer/hmacsha256.rs
@@ -5,7 +5,7 @@ use crate::client::signer::signature::Signature;
 
 pub struct HmacSha256<'a> {
     pub api_key: &'a str,
-    pub secret_key: &'a str,
+    secret_key: &'a str,
 }
 
 impl<'a> HmacSha256<'a> {

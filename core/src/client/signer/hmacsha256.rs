cf1a84e85b790c09abe69d2e9b6c3b26a868294f
Denis Gruia <denis.gruiax@icloud.com>
2025-04-26 11:23:40 +0300

    Added sign and build request for signature trait and implemented them for HmacSha256 data structure

--- core/src/client/signer/hmacsha256.rs
+++ core/src/client/signer/hmacsha256.rs
@@ -4,7 +4,7 @@ use sha2::Sha256;
 use crate::client::signer::signature::Signature;
 
 pub struct HmacSha256<'a> {
-    pub api_key: &'a str,
+    api_key: &'a str,
     secret_key: &'a str,
 }
 
@@ -18,11 +18,21 @@ impl<'a> HmacSha256<'a> {
 }
 
 impl<'a> Signature<'a> for HmacSha256<'a> {
-    fn sign(&self, params: &str) -> String {
-        let mut hasher = Hmac::<Sha256>::new_from_slice(self.secret_key.as_bytes()).unwrap();
-        hasher.update(params.as_bytes());
-        let result = hasher.finalize().into_bytes();
+    fn build_request(&self, host: &str, path: &str, params: &str) -> reqwest::RequestBuilder{
+        let url = self.sign(host, path, params);
 
-        hex::encode(result)
+        reqwest::Client::new().get(url).header("X-MBX-APIKEY", self.api_key)
+    } 
+
+    fn sign(&self, host: &str, path: &str, params: &str) -> String {
+        let mut hasher = Hmac::<Sha256>::new_from_slice(self.secret_key.as_bytes()).unwrap();
+        let params = self.add_timestamp(params);
+
+        hasher.update(params.as_bytes());
+
+        let signature = hex::encode(hasher.finalize().into_bytes());
+        let endpoint = format!("{}&signature={}", params, signature);
+
+        format!("{}{}{}", host, path, endpoint)
     }
 }

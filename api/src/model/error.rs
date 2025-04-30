e185bfa9695a236eb4a9c3994bfd690733d703d0
Denis Gruia <denis.gruiax@icloud.com>
2025-04-30 10:43:18 +0300

    Implemented Display tarit for BinanceError

--- api/src/model/error.rs
+++ api/src/model/error.rs
@@ -1,7 +1,14 @@
 use serde::Deserialize;
+use std::fmt::Display;
 
 #[derive(Debug, Deserialize)]
-pub struct BinanceError{
+pub struct BinanceError {
     pub code: i16,
     pub msg: String,
+}
+
+impl Display for BinanceError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "code: {},\nmsg: {},\n", self.code, self.msg)
+    }
 }

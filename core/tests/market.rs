346d48f9186b5371068338b955b420331702be55
Denis Gruia <denis.gruiax@icloud.com>
2025-05-01 20:36:23 +0300

    Implemented integrat test for Recent Trade List with a vector of Recent Trade. The data strcuture could be better, but i have to check how to deserialize in structure that holds a vector of trades

--- core/tests/market.rs
+++ core/tests/market.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod market_integration {
     use binance_api::endpoint::route::Market;
+    use binance_api::model::params::market::RecentTradeListParams;
+    use binance_api::model::response::market::RecentTradeResponse;
     use binance_api::{
         endpoint::host::Host,
         model::{params::market::OrderBookParams, response::market::OrderBookResponse},
@@ -24,5 +26,32 @@ mod market_integration {
         assert!(order_book.last_update_id > 0);
         assert_eq!(order_book.bids.len(), 10);
         assert_eq!(order_book.asks.len(), 10);
+    }
+
+    #[tokio::test]
+    async fn test_recent_trade_list() {
+        let params = RecentTradeListParams {
+            symbol: "SOLUSDC",
+            limit: Some(20),
+        };
+
+        let client = Client::new(Host::Api.into(), HmacSha256::new("api_key", "secret_key"));
+
+        let response = client.get(Market::RecentTradeList, params);
+        let body = response.await.unwrap().text().await.unwrap();
+
+        let recent_trade_list =
+            serde_json::from_str::<Vec<RecentTradeResponse>>(&body).unwrap();
+
+        let check_trade = |trade: &RecentTradeResponse| {
+            trade.id > 0
+                && trade.price > 0.0
+                && trade.qty > 0.0
+                && trade.quote_qty > 0.0
+                && trade.time > 0
+        };
+
+        assert_eq!(recent_trade_list.len(), 20);
+        assert!(recent_trade_list.iter().all(check_trade));
     }
 }

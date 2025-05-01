5f4a7ddcfb1a2cea5ea5a64b41532a605508b4c3
Denis Gruia <denis.gruiax@icloud.com>
2025-05-01 10:51:27 +0300

    Updated Binnace Api Errors

--- api/src/model/error.rs
+++ api/src/model/error.rs
@@ -1,14 +1,29 @@
 use serde::Deserialize;
 use std::fmt::Display;
 
+#[derive(Debug)]
+pub enum BinanceError {
+    Error(ApiError),
+    BuildRequestError(String),
+    HttpRequest(reqwest::Error),
+    Other(String),
+}
+
 #[derive(Debug, Deserialize)]
-pub struct BinanceError {
-    pub code: i16,
-    pub msg: String,
+pub struct ApiError {
+    code: i16,
+    msg: String,
 }
 
 impl Display for BinanceError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "code: {},\nmsg: {},\n", self.code, self.msg)
+        match self {
+            BinanceError::Error(ApiError { code, msg }) => {
+                write!(f, "code: {},\nmsg: {},\n", code, msg)
+            }
+            BinanceError::BuildRequestError(msg) => write!(f, "Build request error: {}", msg),
+            BinanceError::HttpRequest(request) => write!(f, "Http response error: {}", request),
+            BinanceError::Other(sig) => write!(f, "Signature build error: {}", sig),
+        }
     }
 }

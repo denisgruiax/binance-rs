d925f965be109cd49857b4dbea8fc057c4939c82
Denis Gruia <denis.gruiax@icloud.com>
2025-05-01 10:57:42 +0300

    Asynchronous Client: updated the build request function and added a new associated function handle for unit testing the code, it is not the final version

--- core/src/client/asynchronous.rs
+++ core/src/client/asynchronous.rs
@@ -1,6 +1,7 @@
 use crate::client::signer::signature::Signature;
-use binance_api::model::params::url::UrlEncoded;
-use reqwest::{RequestBuilder, Response};
+use binance_api::model::{BinanceError, params::url::UrlEncoded};
+use reqwest::{RequestBuilder, Response, StatusCode};
+use serde::de::DeserializeOwned;
 
 pub struct Client<'a, S>
 where
@@ -33,7 +34,7 @@ where
         Params: UrlEncoded,
     {
         let endpoint = format!("{}{}{}", self.host, path.into(), params.to_url_encoded());
-        
+
         self.inner_client.get(endpoint).send()
     }
 
@@ -46,10 +47,40 @@ where
         Path: Into<&'a str>,
         Params: UrlEncoded,
     {
-        let request = self
-            .signature
-            .build_request(&self.inner_client,self.host, path.into(), params.to_url_encoded().as_str());
+        let request = self.signature.build_request(
+            &self.inner_client,
+            self.host,
+            path.into(),
+            params.to_url_encoded().as_str(),
+        );
 
         RequestBuilder::send(request)
+    }
+
+    pub async fn handle<T: DeserializeOwned>(
+        response: impl Future<Output = Result<Response, reqwest::Error>>,
+    ) -> Result<T, BinanceError> {
+        match response.await {
+            Ok(response) => match response.status() {
+                StatusCode::OK => Ok(serde_json::from_str::<T>(&response.text().await.unwrap())
+                    .map_err(|_| {
+                        BinanceError::Other(String::from(
+                            "Deserialize error from String to data type T",
+                        ))
+                    }))?,
+
+                StatusCode::BAD_REQUEST => {
+                    let res = response.text().await.unwrap();
+                    let api_error: binance_api::model::error::ApiError =
+                        serde_json::from_str(&res).unwrap();
+                    Err(BinanceError::Error(api_error))
+                }
+                status_code => Err(BinanceError::Other(format!(
+                    "Response Status Code: {}",
+                    status_code
+                ))),
+            },
+            Err(error) => Err(BinanceError::HttpRequest(error)),
+        }
     }
 }

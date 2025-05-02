603303d696c1bca43965f63dfa723f2ed5da03ab
Denis Gruia <denis.gruiax@icloud.com>
2025-05-02 03:04:50 +0300

    Implemented parameters for Old Trade Lookup API

--- api/src/model/params/market.rs
+++ api/src/model/params/market.rs
@@ -9,9 +9,21 @@ pub struct OrderBookParams<'a> {
 }
 
 #[derive(Serialize)]
-pub struct RecentTradeListParams<'a>{
+pub struct RecentTradeListParams<'a> {
     pub symbol: &'a str,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub limit: Option<u16>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OldTradeLookupParams<'a> {
+    pub symbol: &'a str,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limit: Option<u16>,
+    
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub from_id: Option<u64>,
 }

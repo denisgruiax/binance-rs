ef1516e2acac61ec81320216c5e0fcc45fdcc9da
Denis Gruia <denis.gruiax@icloud.com>
2025-05-02 12:25:42 +0300

    Implemented Parameters data structure for Klines/Candlesticks

--- api/src/model/params/market.rs
+++ api/src/model/params/market.rs
@@ -23,7 +23,26 @@ pub struct OldTradeLookupParams<'a> {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub limit: Option<u16>,
-    
+
     #[serde(skip_serializing_if = "Option::is_none")]
     pub from_id: Option<u64>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KlineCandlestickDataParams<'a> {
+    pub symbol: &'a str,
+    pub interval: &'a str,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub start_time: Option<u64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub end_time: Option<u64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub time_zone: Option<&'a str>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limit: Option<u16>,
 }

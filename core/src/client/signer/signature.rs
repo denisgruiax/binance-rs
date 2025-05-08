af6ec4ef930feb2357c03fe709a83453d00d18e5
Denis Gruia <denis.gruiax@icloud.com>
2025-05-08 17:19:12 +0300

    Changed the trait to build a synchronous client

--- core/src/client/signer/signature.rs
+++ core/src/client/signer/signature.rs
@@ -6,6 +6,21 @@ pub trait Signature<'a> {
         format!("{}&timestamp={}", params, Utc::now().timestamp_millis())
     }
 
-    fn build_request(&self, client: &reqwest::Client, host: &str, path: &str, params: &str) -> Result<reqwest::RequestBuilder, BinanceError>;
+    fn build_blocking_request(
+        &self,
+        client: &reqwest::blocking::Client,
+        host: &str,
+        path: &str,
+        params: &str,
+    ) -> Result<reqwest::blocking::RequestBuilder, BinanceError>;
+    
+    fn build_request(
+        &self,
+        client: &reqwest::Client,
+        host: &str,
+        path: &str,
+        params: &str,
+    ) -> Result<reqwest::RequestBuilder, BinanceError>;
+    
     fn sign(&self, host: &str, path: &str, params: &str) -> Result<String, BinanceError>;
 }

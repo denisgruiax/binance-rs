365c1f9edf5a59ba14da75c469f71281777b9541
Denis Gruia <denis.gruiax@icloud.com>
2025-05-08 17:19:43 +0300

    Implementation for build_blocking_function from Signature trait

--- core/src/client/signer/hmacsha256.rs
+++ core/src/client/signer/hmacsha256.rs
@@ -5,7 +5,7 @@ use sha2::Sha256;
 use crate::client::signer::signature::Signature;
 
 pub struct HmacSha256<'a> {
-    api_key: &'a str,
+    pub api_key: &'a str,
     secret_key: &'a str,
 }
 
@@ -19,11 +19,29 @@ impl<'a> HmacSha256<'a> {
 }
 
 impl<'a> Signature<'a> for HmacSha256<'a> {
-    fn build_request(&self, client: &reqwest::Client, host: &str, path: &str, params: &str) -> Result<reqwest::RequestBuilder, BinanceError>{
+    fn build_blocking_request(
+        &self,
+        client: &reqwest::blocking::Client,
+        host: &str,
+        path: &str,
+        params: &str,
+    ) -> Result<reqwest::blocking::RequestBuilder, BinanceError> {
         let url = self.sign(host, path, params)?;
 
         Ok(client.get(url).header("X-MBX-APIKEY", self.api_key))
-    } 
+    }
+
+    fn build_request(
+        &self,
+        client: &reqwest::Client,
+        host: &str,
+        path: &str,
+        params: &str,
+    ) -> Result<reqwest::RequestBuilder, BinanceError> {
+        let url = self.sign(host, path, params)?;
+
+        Ok(client.get(url).header("X-MBX-APIKEY", self.api_key))
+    }
 
     fn sign(&self, host: &str, path: &str, params: &str) -> Result<String, BinanceError> {
         let mut hasher = Hmac::<Sha256>::new_from_slice(self.secret_key.as_bytes())?;

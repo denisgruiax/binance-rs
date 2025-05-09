6fdd21259d786096de6c8390c65c8a42fbbbc034
Denis Gruia <denis.gruiax@icloud.com>
2025-05-09 11:00:18 +0300

    Implemneted depth api and unit test for it

--- core/src/spot/market.rs
+++ core/src/spot/market.rs
@@ -1,5 +1,4 @@
 use crate::client::{signer::signature::Signature, synchronous::Client};
-use binance_api::endpoint::host::Host;
 use binance_api::endpoint::route::Market;
 use binance_api::model::params::market::*;
 use binance_api::model::{BinanceError, response::market::DepthResponse};
@@ -17,5 +16,52 @@ where
 {
     pub fn new(client: Client<'a, S>) -> Self {
         MarketApi { client }
+    }
+
+    pub fn depth(&self, params: DepthParams) -> Result<DepthResponse, BinanceError> {
+        self.client.get(Market::Depth.into(), params)
+    }
+}
+
+#[cfg(test)]
+mod market_api {
+    use binance_api::{
+        endpoint::host::Host,
+        model::{params::market::DepthParams, response::market::DepthResponse},
+    };
+    use futures::stream::Once;
+
+    use std::sync::{Arc, OnceLock};
+
+    use crate::client::{signer::hmacsha256::HmacSha256, synchronous::Client};
+
+    use super::MarketApi;
+
+    static CLIENT: OnceLock<Arc<MarketApi<'static, HmacSha256<'static>>>> = OnceLock::new();
+
+    fn shared_test_market() -> Arc<MarketApi<'static, HmacSha256<'static>>> {
+        CLIENT
+            .get_or_init(|| {
+                Arc::new(MarketApi::new(Client::new(
+                    Host::Api.into(),
+                    HmacSha256::new("api_key", "secret_key"),
+                )))
+            })
+            .clone()
+    }
+
+    #[test]
+    fn test_depth() {
+        let market_api = shared_test_market();
+        let params = DepthParams {
+            symbol: "BTCUSDC",
+            limit: Some(20),
+        };
+
+        let depth: DepthResponse = market_api.depth(params).unwrap();
+
+        assert!(depth.last_update_id > 0);
+        assert_eq!(depth.bids.len(), 20);
+        assert_eq!(depth.asks.len(), 20);
     }
 }

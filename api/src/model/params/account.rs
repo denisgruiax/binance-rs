c4850b410034962502f1bd4feaa919ab250c7de2
Denis Gruia <denis.gruiax@icloud.com>
2025-05-17 00:44:27 +0300

    Implemented parameters data structures for My Trades endpoint in Spot Account Api

--- api/src/model/params/account.rs
+++ api/src/model/params/account.rs
@@ -6,3 +6,15 @@ pub struct InfoParams {
     pub omit_zero_balances: Option<bool>,
     pub recv_window: Option<u64>,
 }
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MyTradesParams<'a> {
+    pub symbol: &'a str,
+    pub order_id: Option<u64>,
+    pub start_time: Option<u64>,
+    pub end_time: Option<u64>,
+    pub from_id: Option<u64>,
+    pub limit: Option<u16>,
+    pub recv_window: Option<u16>,
+}

b7dfe7d79964f8942caf989cd23624edc48352ef
Denis Gruia <denis.gruiax@icloud.com>
2025-05-20 14:44:24 +0300

    Added result response data structures for new order endpoint

--- api/src/model/response/trading.rs
+++ api/src/model/response/trading.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -8,4 +9,37 @@ pub struct AckResponse {
     order_list_id: i64,
     client_order_id: String,
     transact_time: u64,
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ResultResponse {
+    pub symbol: String,
+    pub order_id: u64,
+    pub order_list_id: i64,
+    pub client_order_id: String,
+    pub transact_time: u64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub price: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub orig_qty: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub executed_qty: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub orig_quote_order_qty: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub cummulative_quote_qty: f64,
+
+    pub status: String,
+    pub time_in_force: String,
+    pub r#type: String,
+    pub side: String,
+    pub working_time: u64,
+    pub self_trade_prevention_mode: String,
 }

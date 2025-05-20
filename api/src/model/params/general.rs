fdebf9ed1918eed889febfb993d265f8836e3476
Denis Gruia <denis.gruiax@icloud.com>
2025-05-20 19:53:42 +0300

    Added implementation for ExchangeInformationParams

--- api/src/model/params/general.rs
+++ api/src/model/params/general.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-#[derive(Serialize)]
+#[derive(Default, Serialize)]
 pub struct ExchangeInformationParams<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub symbol: Option<&'a str>,
@@ -10,4 +10,25 @@ pub struct ExchangeInformationParams<'a> {
 
     #[serde(skip_serializing_if = "Option::is_none")]
     pub permissions: Option<&'a str>,
+}
+
+impl<'a> ExchangeInformationParams<'a> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = Some(symbol);
+        self
+    }
+
+    pub fn symbols(mut self, symbols: &'a str) -> Self {
+        self.symbols = Some(symbols);
+        self
+    }
+
+    pub fn permissions(mut self, permissions: &'a str) -> Self {
+        self.permissions = Some(permissions);
+        self
+    }
 }

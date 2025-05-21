8e92a1d1241617e9231352f31e54be94e743d5fc
Denis Gruia <denis.gruiax@icloud.com>
2025-05-22 01:27:28 +0300

    Implemented build pattern for new order params data structure and also added 2 additional functions for creating a new order

--- api/src/model/params/trading.rs
+++ api/src/model/params/trading.rs
@@ -23,7 +23,96 @@ pub struct NewOrderParams<'a> {
 }
 
 impl<'a> NewOrderParams<'a> {
-    fn limit(
+    pub fn new(symbol: &'a str, side: OrderSide, r#type: OrderType) -> Self {
+        NewOrderParams {
+            symbol,
+            side,
+            r#type,
+            ..Default::default()
+        }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = symbol;
+        self
+    }
+
+    pub fn side(mut self, side: OrderSide) -> Self {
+        self.side = side;
+        self
+    }
+
+    pub fn r#type(mut self, r#type: OrderType) -> Self {
+        self.r#type = r#type;
+        self
+    }
+
+    pub fn time_in_force(mut self, time_in_force: TimeInForce) -> Self {
+        self.time_in_force = Some(time_in_force);
+        self
+    }
+
+    pub fn quantity(mut self, quantity: f64) -> Self {
+        self.quantity = Some(quantity);
+        self
+    }
+
+    pub fn quote_order_qty(mut self, quote_order_qty: f64) -> Self {
+        self.quote_order_qty = Some(quote_order_qty);
+        self
+    }
+
+    pub fn price(mut self, price: f64) -> Self {
+        self.price = Some(price);
+        self
+    }
+
+    pub fn new_client_order_id(mut self, new_client_order_id: &'a str) -> Self {
+        self.new_client_order_id = Some(new_client_order_id);
+        self
+    }
+
+    pub fn strategy_id(mut self, strategy_id: u64) -> Self {
+        self.strategy_id = Some(strategy_id);
+        self
+    }
+
+    pub fn strategy_type(mut self, strategy_type: u64) -> Self {
+        self.strategy_type = Some(strategy_type);
+        self
+    }
+
+    pub fn stop_price(mut self, stop_price: f64) -> Self {
+        self.stop_price = Some(stop_price);
+        self
+    }
+
+    pub fn trailing_delta(mut self, trailing_delta: u64) -> Self {
+        self.trailing_delta = Some(trailing_delta);
+        self
+    }
+
+    pub fn icerberg_qty(mut self, icerberg_qty: f64) -> Self {
+        self.icerberg_qty = Some(icerberg_qty);
+        self
+    }
+
+    pub fn new_order_resp_type(mut self, new_order_resp_type: OrderResponseType) -> Self {
+        self.new_order_resp_type = Some(new_order_resp_type);
+        self
+    }
+
+    pub fn self_trade_prevention_mode(mut self, self_trade_prevention_mode: StpModes) -> Self {
+        self.self_trade_prevention_mode = Some(self_trade_prevention_mode);
+        self
+    }
+
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
+        self.recv_window = Some(recv_window);
+        self
+    }
+
+    pub fn limit(
         symbol: &'a str,
         side: OrderSide,
         r#type: OrderType,
@@ -42,12 +131,7 @@ impl<'a> NewOrderParams<'a> {
         }
     }
 
-    fn market(
-        symbol: &'a str,
-        side: OrderSide,
-        r#type: OrderType,
-        quote_order_qty: f64,
-    ) -> Self {
+    fn market(symbol: &'a str, side: OrderSide, r#type: OrderType, quote_order_qty: f64) -> Self {
         NewOrderParams {
             symbol,
             side,

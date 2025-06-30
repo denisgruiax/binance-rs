6a1a50ae1ea438ec5e522f97141571b74f97f3dd
Denis Gruia <denis.gruiax@icloud.com>
2025-06-30 16:20:39 +0300

    Refactor market module: reorganize imports and remove unused test code

--- binance-futures/src/market.rs
+++ binance-futures/src/market.rs
@@ -1,11 +1,16 @@
 use binance_common::error::BinanceError;
 use binance_common::futures::endpoint::route::Market;
-use binance_common::futures::model::response::market::DepthResponse;
+use binance_common::futures::model::response::market::{
+    DepthResponse, HistoricalTradesResponse, TradesResponse,
+};
 use binance_common::futures::model::{
-    params::market::{DepthParams, EmptyParams, ExchangeInformationParams},
+    params::market::{
+        DepthParams, EmptyParams, ExchangeInformationParams, HistoricalTradesParams, TradesParams,
+    },
     response::market::{EmptyResponse, ExchangeInformationResponse, ServerTimeResponse},
 };
 use binance_core::{client::synchronous::Client, signer::signature::Signature};
+use reqwest::Method;
 
 pub struct MarketApi<'a, S>
 where
@@ -40,83 +45,16 @@ where
     pub fn get_depth(&self, params: DepthParams) -> Result<DepthResponse, BinanceError> {
         self.client.get(Market::Depth, params)
     }
-}
 
-#[cfg(test)]
-mod futures_market_api {
-    use super::MarketApi;
-    use binance_common::futures::model::params::market::DepthParams;
-    use binance_common::futures::model::response::market::DepthResponse;
-    use binance_common::futures::{
-        endpoint::host::Host,
-        model::{params::market::ExchangeInformationParams, response::market::EmptyResponse},
-    };
-    use binance_core::{
-        client::synchronous::Client,
-        signer::{hmacsha256::HmacSha256, signature::Signature},
-    };
-    use std::sync::{Arc, OnceLock};
-
-    static CLIENT: OnceLock<Arc<MarketApi<'static, HmacSha256<'static>>>> = OnceLock::new();
-
-    fn shared_test_client<'a, S>() -> Arc<MarketApi<'static, HmacSha256<'static>>>
-    where
-        S: Signature<'a>,
-    {
-        CLIENT
-            .get_or_init(|| {
-                Arc::new(MarketApi::new(Client::new(
-                    &Host::Api,
-                    HmacSha256::new("api_key", "secret_key"),
-                )))
-            })
-            .clone()
+    pub fn get_trades(&self, params: TradesParams) -> Result<Vec<TradesResponse>, BinanceError> {
+        self.client.get(Market::Trades, params)
     }
 
-    #[test]
-    fn test_ping() {
-        let market_api: Arc<MarketApi<HmacSha256>> = shared_test_client::<HmacSha256>().clone();
-
-        assert_eq!(market_api.ping().unwrap(), EmptyResponse {});
-    }
-    #[test]
-    fn test_get_server_time() {
-        let market_api: Arc<MarketApi<HmacSha256>> = shared_test_client::<HmacSha256>().clone();
-
-        assert!(market_api.get_server_time().unwrap().server_time > 0);
-    }
-
-    #[test]
-    fn test_get_exchange_info() {
-        let market_api: Arc<MarketApi<HmacSha256>> = shared_test_client::<HmacSha256>();
-
-        let params = ExchangeInformationParams::new().symbol("BTCUSDT");
-
-        let exchange_info = market_api.get_exchange_info(params).unwrap();
-
-        assert_eq!(exchange_info.timezone, "UTC");
-        assert!(exchange_info.server_time > 0);
-
-        assert_eq!(exchange_info.symbols[0].symbol, "BTCUSDT");
-        assert_eq!(exchange_info.symbols[0].status, "TRADING");
-        assert_eq!(exchange_info.symbols[0].base_asset, "BTC");
-        assert_eq!(exchange_info.symbols[0].quote_asset, "USDT");
-        assert_eq!(exchange_info.symbols[0].base_asset_precision, 8);
-        assert!(exchange_info.symbols[0].order_types.len() == 7);
-    }
-
-    #[test]
-    fn test_get_depth() {
-        let market_api: Arc<MarketApi<HmacSha256>> = shared_test_client::<HmacSha256>();
-
-        let params = DepthParams::new("BTCUSDT").limit(5);
-
-        let depth: DepthResponse = market_api.get_depth(params).unwrap();
-
-        assert!(depth.last_update_id > 0);
-        assert!(depth.e > 0);
-        assert!(depth.t > 0);
-        assert_eq!(depth.bids.len(), 5);
-        assert_eq!(depth.asks.len(), 5);
+    pub fn get_historical_trades(
+        &self,
+        params: HistoricalTradesParams,
+    ) -> Result<Vec<HistoricalTradesResponse>, BinanceError> {
+        self.client
+            .send(Market::HistoricalTrades, params, Method::GET)
     }
 }

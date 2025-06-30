278a9549188e64269b256aad239b77c9cfe73a4f
Denis Gruia <denis.gruiax@icloud.com>
2025-07-01 00:20:25 +0300

    Add Ticker24hParams and Ticker24hResponse structs for 24-hour ticker data

--- binance-common/src/futures/model/params/market.rs
+++ binance-common/src/futures/model/params/market.rs
@@ -2,7 +2,7 @@ pub use crate::spot::model::params::market::{
     DepthParams, HistoricalTradesParams, KlinesParams, TradesParams,
 };
 pub use crate::spot::model::params::{EmptyParams, general::ExchangeInformationParams};
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
 #[derive(Clone, Debug, Default, Serialize)]
 pub struct MarkPriceParams<'a> {
@@ -22,10 +22,26 @@ impl<'a> MarkPriceParams<'a> {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Serialize)]
 pub struct FundingRateHistoryParams<'a> {
     pub symbol: Option<&'a str>,
     pub start_time: Option<u64>,
     pub end_time: Option<u64>,
     pub limit: Option<u16>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Ticker24hParams<'a> {
+    pub symbol: &'a str,
+}
+
+impl<'a> Ticker24hParams<'a> {
+    pub fn new(symbol: &'a str) -> Self {
+        Ticker24hParams { symbol }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = symbol;
+        self
+    }
 }

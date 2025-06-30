8b2afaf7544dea00da9d2a8785ecb751b0b3e50b
Denis Gruia <denis.gruiax@icloud.com>
2025-06-30 16:59:11 +0300

    Add MarkPriceParams struct for market parameters in futures module

--- binance-common/src/futures/model/params/market.rs
+++ binance-common/src/futures/model/params/market.rs
@@ -1,2 +1,23 @@
-pub use crate::spot::model::params::market::{DepthParams, TradesParams, HistoricalTradesParams, KlinesParams};
+pub use crate::spot::model::params::market::{
+    DepthParams, HistoricalTradesParams, KlinesParams, TradesParams,
+};
 pub use crate::spot::model::params::{EmptyParams, general::ExchangeInformationParams};
+use serde::Serialize;
+
+#[derive(Clone, Debug, Default, Serialize)]
+pub struct MarkPriceParams<'a> {
+    pub symbol: Option<&'a str>,
+}
+
+impl<'a> MarkPriceParams<'a> {
+    pub fn new(symbol: &'a str) -> Self {
+        MarkPriceParams {
+            symbol: Some(symbol),
+        }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = Some(symbol);
+        self
+    }
+}

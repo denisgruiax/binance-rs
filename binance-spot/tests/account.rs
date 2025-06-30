d25959b14b44e6ef3f1db0380d142c82733ec039
Denis Gruia <denis.gruiax@icloud.com>
2025-06-30 13:43:08 +0300

    New modules for integration tests

---
+++ binance-spot/tests/account.rs
@@ -0,0 +1,78 @@
+#[cfg(test)]
+mod spot_account_api_integration_tests {
+    use binance_common::spot::endpoint::host::Host;
+    use binance_common::spot::model::params::account::{
+        InfoParams, MyTradesParams, UnfilledOrderCountParams,
+    };
+    use binance_common::spot::model::response::account::{
+        InfoResponse, MyTradesResponse, UnfilledOrderCountResponse,
+    };
+    use binance_core::{client::synchronous::Client, signer::hmacsha256::HmacSha256};
+    use binance_spot::{
+        account::AccountApi,
+        secret::{API_KEY, SECRET_KEY},
+    };
+    use std::sync::{Arc, OnceLock};
+    static CLIENT: OnceLock<Arc<AccountApi<'static, HmacSha256<'static>>>> = OnceLock::new();
+
+    fn shared_test_account() -> Arc<AccountApi<'static, HmacSha256<'static>>> {
+        CLIENT
+            .get_or_init(|| {
+                Arc::new(AccountApi::new(Client::new(
+                    &Host::Api,
+                    HmacSha256::new(API_KEY, SECRET_KEY),
+                )))
+            })
+            .clone()
+    }
+
+    #[test]
+    fn test_get_info() {
+        let account_api = shared_test_account();
+
+        let params = InfoParams::new().omit_zero_balances(true).recv_window(5000);
+
+        let info: InfoResponse = account_api.get_info(params).unwrap();
+
+        assert!(info.can_trade);
+        assert!(info.can_withdraw);
+        assert!(info.can_deposit);
+        assert!(info.update_time > 0);
+        assert!(info.uid > 0);
+    }
+
+    #[test]
+    fn test_get_my_trades() {
+        let account_api = shared_test_account();
+
+        let params = MyTradesParams::new("BNBUSDC");
+
+        let my_trades: Vec<MyTradesResponse> = account_api.get_my_trades(params).unwrap();
+
+        let check_trade = |trade: &MyTradesResponse| {
+            trade.id > 0
+                && trade.order_id > 0
+                && trade.price > 0.0
+                && trade.qty > 0.0
+                && trade.time > 0
+        };
+
+        assert!(my_trades.iter().all(check_trade));
+    }
+
+    #[test]
+    fn test_unfilled_order_count() {
+        let account_api = shared_test_account();
+        let unffiled_order_count: Vec<UnfilledOrderCountResponse> = account_api
+            .get_unfilled_order_count(UnfilledOrderCountParams::new())
+            .unwrap();
+
+        assert!(
+            unffiled_order_count
+                .iter()
+                .all(|order| order.interval_num > 0
+                    && order.limit > 0
+                    && (order.count > 0 || order.count == 0))
+        )
+    }
+}
---
+++ binance-spot/tests/trade.rs
@@ -0,0 +1,132 @@
+#[cfg(test)]
+mod spot_trade_api_integration_tests {
+    use binance_spot::market::MarketApi;
+    use binance_spot::secret::{API_KEY, SECRET_KEY};
+    use binance_spot::trade::TradingApi;
+
+    use binance_common::enums::{OrderResponseType, OrderSide};
+    use binance_common::spot::endpoint::host::Host;
+    use binance_common::spot::model::params::trade::NewOrderParams;
+    use binance_core::{client::synchronous::Client, signer::hmacsha256::HmacSha256};
+    use std::sync::{Arc, OnceLock};
+
+    static MARKET_CLIENT: OnceLock<Arc<MarketApi<'static, HmacSha256<'static>>>> = OnceLock::new();
+    static TRADING_CLIENT: OnceLock<Arc<TradingApi<'static, HmacSha256<'static>>>> =
+        OnceLock::new();
+    static SYMBOL: &'static str = "BTCUSDC";
+
+    fn shared_test_trading() -> Arc<TradingApi<'static, HmacSha256<'static>>> {
+        TRADING_CLIENT
+            .get_or_init(|| {
+                Arc::new(TradingApi::new(Client::new(
+                    &Host::Api,
+                    HmacSha256::new(API_KEY, SECRET_KEY),
+                )))
+            })
+            .clone()
+    }
+
+    fn shared_test_market() -> Arc<MarketApi<'static, HmacSha256<'static>>> {
+        MARKET_CLIENT
+            .get_or_init(|| {
+                Arc::new(MarketApi::new(Client::new(
+                    &Host::Api,
+                    HmacSha256::new(API_KEY, SECRET_KEY),
+                )))
+            })
+            .clone()
+    }
+
+    #[test]
+    fn test_post_new_market_order() {
+        let trading_api = shared_test_trading();
+
+        let params = NewOrderParams::market(SYMBOL, OrderSide::Buy, 1000.0)
+            .new_order_resp_type(OrderResponseType::Ack);
+
+        let response = trading_api.post_new_test_order(params);
+
+        match response {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_post_new_limit_order() {
+        let market_api = shared_test_market();
+        let trading_api = shared_test_trading();
+
+        let btc_usdc_price = market_api.get_price_ticker(SYMBOL).unwrap().price;
+        let limit_price = (btc_usdc_price - (btc_usdc_price * 0.05)).round();
+
+        let params = NewOrderParams::limit(SYMBOL, OrderSide::Buy, limit_price, 1.0)
+            .new_order_resp_type(OrderResponseType::Ack);
+
+        let response = trading_api.post_new_test_order(params);
+
+        match response {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_post_new_stop_loss_order() {
+        let market_api = shared_test_market();
+        let trading_api = shared_test_trading();
+
+        let btc_usdc_price = market_api.get_price_ticker(SYMBOL).unwrap().price;
+        let stop_price = (btc_usdc_price - (btc_usdc_price * 0.02)).round();
+
+        let params = NewOrderParams::stop_loss(SYMBOL, OrderSide::Buy, 0.1, stop_price)
+            .new_order_resp_type(OrderResponseType::Ack);
+
+        let response = trading_api.post_new_test_order(params);
+
+        match response {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_post_new_take_profit_order() {
+        let market_api = shared_test_market();
+        let trading_api = shared_test_trading();
+
+        let btc_usdc_price = market_api.get_price_ticker(SYMBOL).unwrap().price;
+        let stop_price = (btc_usdc_price + (btc_usdc_price * 0.1)).round();
+
+        let params = NewOrderParams::take_profit(SYMBOL, OrderSide::Sell, 0.1, stop_price)
+            .new_order_resp_type(OrderResponseType::Ack);
+
+        let response = trading_api.post_new_test_order(params);
+
+        match response {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_post_new_take_profit_limit_order() {
+        let market_api = shared_test_market();
+        let trading_api = shared_test_trading();
+
+        let btc_usdc_price = market_api.get_price_ticker(SYMBOL).unwrap().price;
+        let stop_price = (btc_usdc_price + (btc_usdc_price * 0.08)).round();
+        let sell_price = (btc_usdc_price + (btc_usdc_price * 0.1)).round();
+
+        let params =
+            NewOrderParams::take_profit_limit(SYMBOL, OrderSide::Sell, sell_price, 0.1, stop_price)
+                .new_order_resp_type(OrderResponseType::Ack);
+
+        let response = trading_api.post_new_test_order(params);
+
+        match response {
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
+        }
+    }
+}

c76e59f9847e88c05b8ee7bf161acc02763dd55f
Denis Gruia <denis.gruiax@icloud.com>
2025-07-02 17:29:27 +0300

    Add FuturesBalanceResponse struct for handling futures balance data

---
+++ binance-common/src/futures/model/response/account.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+use serde_with::{DisplayFromStr, serde_as};
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FuturesBalanceResponse {
+    pub account_alias: String,
+    pub asset: String,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub balance: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub cross_wallet_balance: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub cross_un_pnl: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub available_balance: f64,
+
+    #[serde_as(as = "DisplayFromStr")]
+    pub max_withdraw_amount: f64,
+
+    pub margin_available: bool,
+    pub update_time: u64,
+}

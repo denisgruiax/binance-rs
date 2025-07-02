d2997d13da5a2d3fed7648a5e560bbe9b14321f4
Denis Gruia <denis.gruiax@icloud.com>
2025-07-02 13:15:25 +0300

    Refactor OpenInterestHistoryParams to use Option types for limit, start_time, and end_time

--- binance-common/src/futures/model/params/market.rs
+++ binance-common/src/futures/model/params/market.rs
@@ -48,7 +48,42 @@ impl<'a> Pair<'a> {
 pub struct OpenInterestHistoryParams<'a> {
     pub symbol: &'a str,
     pub period: &'a str,
-    pub limit: u16,
-    pub start_time: u64,
-    pub end_time: u64,
+    pub limit: Option<u16>,
+    pub start_time: Option<u64>,
+    pub end_time: Option<u64>,
+}
+
+impl<'a> OpenInterestHistoryParams<'a> {
+    pub fn new(symbol: &'a str, period: &'a impl AsRef<str>) -> Self {
+        OpenInterestHistoryParams {
+            symbol,
+            period: period.as_ref(),
+            ..Default::default()
+        }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = symbol;
+        self
+    }
+
+    pub fn period(mut self, period: &'a impl AsRef<str>) -> Self {
+        self.period = period.as_ref();
+        self
+    }
+
+    pub fn limit(mut self, limit: u16) -> Self {
+        self.limit = Some(limit);
+        self
+    }
+
+    pub fn start_time(mut self, start_time: u64) -> Self {
+        self.start_time = Some(start_time);
+        self
+    }
+
+    pub fn end_time(mut self, end_time: u64) -> Self {
+        self.end_time = Some(end_time);
+        self
+    }
 }

c0248f9a3bcd0a3203716bf910195cfbbb1579d3
Denis Gruia <denis.gruiax@icloud.com>
2025-07-13 22:19:52 +0300

    Refactor TradeApi methods to improve order handling; update response types and add get_order method.

--- binance-futures/src/trade.rs
+++ binance-futures/src/trade.rs
@@ -4,11 +4,11 @@ use binance_common::{
         endpoint::route::Trade,
         model::{
             params::trade::{
-                CancelAllOrdersParams, NewOrderParams, PositionRiskV3Params, SetLeverageParams,
+                CancelAllOrdersParams, CancelOrderParams, GetOrderParams, NewOrderParams,
+                PositionRiskV3Params, SetLeverageParams,
             },
             response::trade::{
-                CancelAllOrdersResponse, NewOrderResponse, PositionRiskV3Response,
-                SetLeverageResponse, TestNewOrderResponse,
+                CancelAllOrdersResponse, GetOrderResponse, OrderResponse, PositionRiskV3Response, SetLeverageResponse, TestOrderResponse
             },
         },
     },
@@ -34,11 +34,11 @@ where
     pub fn send_new_test_order(
         &self,
         params: NewOrderParams,
-    ) -> Result<TestNewOrderResponse, BinanceError> {
+    ) -> Result<TestOrderResponse, BinanceError> {
         self.client.send(Trade::TestNewOrder, params, Method::POST)
     }
 
-    pub fn send_new_order(&self, params: NewOrderParams) -> Result<NewOrderResponse, BinanceError> {
+    pub fn send_new_order(&self, params: NewOrderParams) -> Result<OrderResponse, BinanceError> {
         self.client.send(Trade::NewOrder, params, Method::POST)
     }
 
@@ -49,12 +49,23 @@ where
         self.client.send(Trade::SetLeverage, params, Method::POST)
     }
 
+    pub fn send_cancel_order(
+        &self,
+        params: CancelOrderParams,
+    ) -> Result<OrderResponse, BinanceError> {
+        self.client.send(Trade::CancelOrder, params, Method::DELETE)
+    }
+
     pub fn send_cancel_all_orders(
         &self,
         params: CancelAllOrdersParams,
     ) -> Result<CancelAllOrdersResponse, BinanceError> {
         self.client
             .send(Trade::CancelAllOrders, params, Method::GET)
+    }
+
+    pub fn get_order(&self, params: GetOrderParams) -> Result<GetOrderResponse, BinanceError> {
+        self.client.send(Trade::GetOrder, params, Method::GET)
     }
 
     pub fn get_position_risk_v3(

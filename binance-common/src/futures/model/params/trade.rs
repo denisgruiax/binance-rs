6e28787e2648baa0c221cf2fc883c69c7cca5094
Denis Gruia <denis.gruiax@icloud.com>
2025-07-13 12:26:42 +0300

    Add CancelAllOrdersParams struct for managing cancellation of all orders

--- binance-common/src/futures/model/params/trade.rs
+++ binance-common/src/futures/model/params/trade.rs
@@ -276,3 +276,29 @@ impl<'a> SetLeverageParams<'a> {
         self
     }
 }
+
+#[derive(Clone, Debug, Default, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CancelAllOrdersParams<'a> {
+    pub symbol: &'a str,
+    pub recv_window: Option<u16>,
+}
+
+impl<'a> CancelAllOrdersParams<'a> {
+    pub fn new(symbol: &'a str) -> Self {
+        CancelAllOrdersParams {
+            symbol,
+            ..Default::default()
+        }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = symbol;
+        self
+    }
+
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
+        self.recv_window = Some(recv_window);
+        self
+    }
+}

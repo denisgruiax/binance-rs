a4fae85afd9393b047dc44e4bd0b23caf41cc0f8
Denis Gruia <denis.gruiax@icloud.com>
2025-07-13 22:32:43 +0300

    Add GetOpenOrderParams struct for retrieving open orders; include fields for symbol, order ID, and client order ID

--- binance-common/src/futures/model/params/trade.rs
+++ binance-common/src/futures/model/params/trade.rs
@@ -404,3 +404,41 @@ impl<'a> GetOrderParams<'a> {
         self
     }
 }
+
+#[derive(Clone, Debug, Default, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GetOpenOrderParams<'a> {
+    pub symbol: &'a str,
+    pub order_id: Option<u64>,
+    pub orig_client_order_id: Option<&'a str>,
+    pub recv_window: Option<u16>,
+}
+
+impl<'a> GetOpenOrderParams<'a> {
+    pub fn new(symbol: &'a str) -> Self {
+        GetOpenOrderParams {
+            symbol,
+            ..Default::default()
+        }
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = symbol;
+        self
+    }
+
+    pub fn order_id(mut self, order_id: u64) -> Self {
+        self.order_id = Some(order_id);
+        self
+    }
+
+    pub fn orig_client_order_id(mut self, orig_client_order_id: &'a str) -> Self {
+        self.orig_client_order_id = Some(orig_client_order_id);
+        self
+    }
+
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
+        self.recv_window = Some(recv_window);
+        self
+    }
+}

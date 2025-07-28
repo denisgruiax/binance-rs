4a935af6b529cee9f2e5993ca0fad51cfe3499fd
Denis Gruia <denis.gruiax@icloud.com>
2025-07-29 00:23:47 +0300

    Added MarkPriceResponse for Websocket Market Api

--- binance-common/src/futures/model/response/websocket.rs
+++ binance-common/src/futures/model/response/websocket.rs
@@ -4,6 +4,7 @@ use serde_with::{DisplayFromStr, serde_as};
 #[derive(Debug)]
 pub enum WebSocketResponse {
     Kline(AggTradeResponse),
+    MarkPrice(MarkPriceResponse),
 }
 
 #[serde_as]
@@ -40,4 +41,36 @@ pub struct AggTradeResponse {
 
     #[serde(rename = "m")]
     pub is_buyer_market_maker: bool,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize)]
+pub struct MarkPriceResponse {
+    #[serde(rename = "e")]
+    pub event_type: String,
+
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "p")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub mark_price: f64,
+
+    #[serde(rename = "i")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub index_price: f64,
+
+    #[serde(rename = "P")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub estimated_settle_price: f64,
+
+    #[serde(rename = "r")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub funding_rate: f64,
+
+    #[serde(rename = "T")]
+    pub next_funding_time: u64,
 }

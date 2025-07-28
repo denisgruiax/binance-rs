96b38b2f0b956c7eba0b9a86d0f0992e2ddb62bb
Denis Gruia <denis.gruiax@icloud.com>
2025-07-29 00:30:19 +0300

    Added KlineResponse for Websocket Market Api

--- binance-common/src/futures/model/response/websocket.rs
+++ binance-common/src/futures/model/response/websocket.rs
@@ -3,8 +3,9 @@ use serde_with::{DisplayFromStr, serde_as};
 
 #[derive(Debug)]
 pub enum WebSocketResponse {
-    Kline(AggTradeResponse),
+    AggTrade(AggTradeResponse),
     MarkPrice(MarkPriceResponse),
+    Kline(KlineResponse)
 }
 
 #[serde_as]
@@ -73,4 +74,82 @@ pub struct MarkPriceResponse {
 
     #[serde(rename = "T")]
     pub next_funding_time: u64,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct KlineResponse {
+    #[serde(rename = "e")]
+    pub event_type: String,
+
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "k")]
+    pub kline: Kline,
+}
+
+#[serde_as]
+#[derive(Clone, Debug, Deserialize)]
+pub struct Kline {
+    #[serde(rename = "t")]
+    pub start_time: u64,
+
+    #[serde(rename = "T")]
+    pub close_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "i")]
+    pub interval: String,
+
+    #[serde(rename = "f")]
+    pub first_trade_id: u64,
+
+    #[serde(rename = "L")]
+    pub last_trade_id: u64,
+
+    #[serde(rename = "o")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub open_price: f64,
+
+    #[serde(rename = "c")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub close_price: f64,
+
+    #[serde(rename = "h")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub high_price: f64,
+
+    #[serde(rename = "l")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub low_price: f64,
+
+    #[serde(rename = "v")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub base_asset_volume: f64,
+
+    #[serde(rename = "n")]
+    pub number_of_trades: u64,
+
+    #[serde(rename = "x")]
+    pub is_closed: bool,
+
+    #[serde(rename = "q")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub quote_asset_volume: f64,
+
+    #[serde(rename = "V")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub taker_buy_base_asset_volume: f64,
+
+    #[serde(rename = "Q")]
+    #[serde_as(as = "DisplayFromStr")]
+    pub taker_buy_quote_asset_volume: f64,
+
+    #[serde(rename = "B")]
+    pub ignore: String,
 }

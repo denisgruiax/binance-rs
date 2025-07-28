a8e3b996fd93e58d47bac9852a4b14b92c441ef7
Denis Gruia <denis.gruiax@icloud.com>
2025-07-28 21:10:54 +0300

    Changed the recv window parameter type from u32 to u16 and added IncomeTypeParams data structure

--- binance-common/src/futures/model/params/account.rs
+++ binance-common/src/futures/model/params/account.rs
@@ -1,19 +1,21 @@
 use serde::Serialize;
 
+use crate::enums::IncomeType;
+
 #[derive(Clone, Debug, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FuturesBalanceParams {
-    pub recv_window: Option<u32>,
+    pub recv_window: Option<u16>,
 }
 
 impl FuturesBalanceParams {
-    pub fn new(recv_window: u32) -> Self {
+    pub fn new(recv_window: u16) -> Self {
         FuturesBalanceParams {
             recv_window: Some(recv_window),
         }
     }
 
-    pub fn recv_window(mut self, recv_window: u32) -> Self {
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
         self.recv_window = Some(recv_window);
         self
     }
@@ -22,17 +24,17 @@ impl FuturesBalanceParams {
 #[derive(Clone, Debug, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PositionSideParams {
-    pub recv_window: Option<u32>,
+    pub recv_window: Option<u16>,
 }
 
 impl PositionSideParams {
-    pub fn new(recv_window: u32) -> Self {
+    pub fn new(recv_window: u16) -> Self {
         PositionSideParams {
             recv_window: Some(recv_window),
         }
     }
 
-    pub fn recv_window(mut self, recv_window: u32) -> Self {
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
         self.recv_window = Some(recv_window);
         self
     }
@@ -41,17 +43,17 @@ impl PositionSideParams {
 #[derive(Clone, Debug, Default, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct FuturesAccountParams {
-    pub recv_window: Option<u32>,
+    pub recv_window: Option<u16>,
 }
 
 impl FuturesAccountParams {
-    pub fn new(recv_window: u32) -> Self {
+    pub fn new(recv_window: u16) -> Self {
         FuturesAccountParams {
             recv_window: Some(recv_window),
         }
     }
 
-    pub fn recv_window(mut self, recv_window: u32) -> Self {
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
         self.recv_window = Some(recv_window);
         self
     }
@@ -61,7 +63,7 @@ impl FuturesAccountParams {
 #[serde(rename_all = "camelCase")]
 pub struct CommissionRateParams<'a> {
     pub symbol: &'a str,
-    pub recv_window: Option<u32>,
+    pub recv_window: Option<u16>,
 }
 
 impl<'a> CommissionRateParams<'a> {
@@ -77,7 +79,60 @@ impl<'a> CommissionRateParams<'a> {
         self
     }
 
-    pub fn recv_window(mut self, recv_window: u32) -> Self {
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
+        self.recv_window = Some(recv_window);
+        self
+    }
+}
+
+#[derive(Clone, Debug, Default, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IncomeHistoryParams<'a> {
+    pub symbol: Option<&'a str>,
+    pub income_type: Option<IncomeType>,
+    pub start_time: Option<u64>,
+    pub end_time: Option<u64>,
+    pub page: Option<u16>,
+    pub limit: Option<u16>,
+    pub recv_window: Option<u16>,
+}
+
+impl<'a> IncomeHistoryParams<'a> {
+    pub fn new() -> Self {
+        IncomeHistoryParams::default()
+    }
+
+    pub fn symbol(mut self, symbol: &'a str) -> Self {
+        self.symbol = Some(symbol);
+        self
+    }
+
+    pub fn income_type(mut self, income_type: IncomeType) -> Self {
+        self.income_type = Some(income_type);
+        self
+    }
+
+    pub fn start_time(mut self, start_time: u64) -> Self {
+        self.start_time = Some(start_time);
+        self
+    }
+
+    pub fn end_time(mut self, end_time: u64) -> Self {
+        self.end_time = Some(end_time);
+        self
+    }
+
+    pub fn page(mut self, page: u16) -> Self {
+        self.page = Some(page);
+        self
+    }
+
+    pub fn limit(mut self, limit: u16) -> Self {
+        self.limit = Some(limit);
+        self
+    }
+
+    pub fn recv_window(mut self, recv_window: u16) -> Self {
         self.recv_window = Some(recv_window);
         self
     }

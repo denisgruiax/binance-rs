9a2a83d0c3c5e1e1079426730d8e064beab7034d
Denis Gruia <denis.gruiax@icloud.com>
2025-08-11 19:38:50 +0300

    New structure divided into async and sync

---
+++ binance-futures/tests/asynchronous.rs
@@ -0,0 +1,6 @@
+pub mod asynchronous{
+    pub mod account;
+    pub mod market;
+    pub mod trade;
+    pub mod websocket;
+}
---
+++ binance-futures/tests/synchronous.rs
@@ -0,0 +1,5 @@
+pub mod synchronous {
+    pub mod account;
+    pub mod market;
+    pub mod trade;
+}

6004b6e24994f6af6fa9a106d0d7b46e28aee278
Denis Gruia <denis.gruiax@icloud.com>
2025-09-03 16:45:52 +0300

    New separate futures module for enums

---
+++ binance-common/src/enums/futures.rs
@@ -0,0 +1,165 @@
+use serde::{Deserialize, Deserializer, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ContractType {
+    Perpetual,
+    CurrentMonth,
+    NextMonth,
+    CurrentQuarter,
+    NextQuarter,
+    PerpetualDelivering,
+}
+
+impl Default for ContractType {
+    fn default() -> Self {
+        ContractType::Perpetual
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ContractStatus {
+    PendingTrading,
+    Trading,
+    PreDelivering,
+    Delivering,
+    Delivered,
+    PreSettle,
+    Settling,
+    Close,
+}
+
+pub enum OrderStatus {
+    New,
+    PartiallyFilled,
+    Filled,
+    Canceled,
+    Rejected,
+    Expired,
+    ExpiredInMatch,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum OrderType {
+    Limit,
+    Market,
+    Stop,
+    StopMarket,
+    TakeProfit,
+    TakeProfitMarket,
+    TrailingStopMarket,
+}
+
+impl Default for OrderType {
+    fn default() -> Self {
+        OrderType::Limit
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum OrderResponseType {
+    Ack,
+    Result,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum OrderSide {
+    Buy,
+    Sell,
+}
+
+impl Default for OrderSide {
+    fn default() -> Self {
+        OrderSide::Buy
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum PositionSide {
+    Both,
+    Long,
+    Short,
+}
+
+impl Default for PositionSide {
+    fn default() -> Self {
+        PositionSide::Long
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum TimeInForce {
+    Gtc,
+    Ioc,
+    Fok,
+    Gtx,
+    Gtd,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum WorkingType {
+    MarkPrice,
+    ContractPrice,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum ResponseType {
+    Ack,
+    Result,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum StpModes {
+    ExpireTaker,
+    ExpireBoth,
+    ExpireMaker,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum PriceMatch {
+    None,
+    Opponent,
+    Opponent5,
+    Opponent10,
+    Opponent20,
+    Queue,
+    Queue5,
+    Queue10,
+    Queue20,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum AdlLevel {
+    LowestRisk,
+    LowRisk,
+    Medium,
+    HighRisk,
+    HighestRisk,
+}
+
+impl<'de> Deserialize<'de> for AdlLevel {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let val: u8 = Deserialize::deserialize(deserializer)?;
+        Ok(match val {
+            0 => AdlLevel::LowestRisk,
+            1 => AdlLevel::LowRisk,
+            2 => AdlLevel::Medium,
+            3 => AdlLevel::HighRisk,
+            4 => AdlLevel::HighestRisk,
+            _ => return Err(serde::de::Error::custom("Invalid ADL level")),
+        })
+    }
+}

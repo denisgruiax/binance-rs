2f54b1f44bf4d7bc0f29bb2f17591a95318556a6
Denis Gruia <denis.gruiax@icloud.com>
2026-03-26 23:53:55 +0200

    Connect function defined in WebSocket trait

--- binance-core/src/websocket.rs
+++ binance-core/src/websocket.rs
@@ -1,8 +1,13 @@
 pub mod futures;
 pub mod spot;
 
-pub trait WebSocket{
+pub trait WebSocket {
     type Command;
     type Error;
     type Response;
+
+    fn connect(
+        &mut self,
+        stream: String,
+    ) -> impl Future<Output = Result<(), Self::Error>> + Send + 'static;
 }

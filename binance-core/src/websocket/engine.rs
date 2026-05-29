d5cf990032bd3802cdb220a2e82e153de1529f2f
Denis Gruia <denis.gruiax@icloud.com>
2026-05-29 21:58:25 +0300

    New engine trait

---
+++ binance-core/src/websocket/engine.rs
@@ -0,0 +1,18 @@
+pub trait WebSocketEngine {
+    type Command;
+    type Error;
+    type Response;
+
+    fn connect(&mut self, stream: String) -> impl Future<Output = Result<(), Self::Error>> + Send;
+
+    fn close(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn disconnect(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
+
+    fn handle(
+        &self,
+        message: tokio_tungstenite::tungstenite::Utf8Bytes,
+    ) -> impl Future<Output = Result<Self::Response, Self::Error>> + Send;
+
+    fn select_action(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn run(&mut self) -> impl Future<Output = ()> + Send;
+}

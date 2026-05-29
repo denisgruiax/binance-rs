7a71e9c4594df54cd97dcf4d2ee4ee8d5e6e4659
Denis Gruia <denis.gruiax@icloud.com>
2026-05-29 21:58:36 +0300

    Handler trait added

---
+++ binance-core/src/websocket/handler.rs
@@ -0,0 +1,21 @@
+use binance_common::enums::WebSocketType;
+
+pub trait WebSocketHandler {
+    type Error;
+    type Response;
+    type State;
+
+    fn new(websocket_type: WebSocketType) -> Self;
+    fn start(&mut self, route: String) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn stop(&mut self) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn restart(&mut self, route: String) -> impl Future<Output = Result<(), Self::Error>> + Send;
+    fn get_state(&mut self) -> impl Future<Output = Result<Self::State, Self::Error>> + Send;
+    fn watch(
+        &mut self,
+    ) -> impl Future<
+        Output = Result<
+            tokio::sync::watch::Receiver<Result<Self::Response, Self::Error>>,
+            Self::Error,
+        >,
+    > + Send;
+}
